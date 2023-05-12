94c651c839883d3b1bf7fec270efde7af0cb4ac0
SunYoung Oh <tjsdud7379@gmail.com>
2023-05-12 22:57:13 +0900

    Feat show modal on modification completion

---
+++ src/components/ResultDialog.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-dialog v-model="dialog" max-width="290">
+    <v-card>
+      <v-card-title class="text-h6">
+        <span class="mr-1">수정을</span>
+        <span v-if="result == 'success'" class="font-weight-bold success--text">
+          성공
+        </span>
+        <span v-else class="red--text text--lighten-1">실패</span>
+        하였습니다.
+      </v-card-title>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="grey darken-3" text @click="$emit('close')"> 닫기 </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: {
+      type: Boolean,
+      default: false,
+    },
+    result: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
--- src/views/Item.vue
+++ src/views/Item.vue
@@ -1,44 +1,80 @@
 <template>
   <v-container>
-    <div>
-      <v-img height="250" width="250" :src="item.images?.basicImage"></v-img>
-      <h2 class="mt-4">{{ item.name }}</h2>
-    </div>
-    <!-- <v-text-field readonly :value="item.contentType"></v-text-field> -->
-    <v-textarea
-      text-narrow
-      class="mt-4 text-body-2"
-      label="상품 상세 설명"
-      auto-grow
-      filled
-      background-color="grey lighten-4"
-      v-model="content"
-    ></v-textarea>
-    <v-btn @click="editItem">수장힉;</v-btn>
+    <v-row justify="center">
+      <v-col cols="auto">
+        <div class="title">
+          <v-img
+            class="title-img"
+            :src="item.images?.basicImage"
+            width="550"
+          ></v-img>
+          <div class="title-text text-h6 mt-6">{{ item.name }}</div>
+        </div>
+        <p class="mt-4 mb-0 text-subtitle-2">상품 상세 설명</p>
+        <v-textarea
+          v-model="content"
+          hide-details
+          text-narrow
+          class="mt-2 mb-4 text-body-2"
+          auto-grow
+          filled
+          background-color="grey lighten-4"
+        ></v-textarea>
+        <v-btn
+          color="primary"
+          dark
+          block
+          large
+          @click="editItem"
+          @click.stop="dialog = true"
+        >
+          수정하기
+        </v-btn>
+        <ResultDialog
+          :dialog="dialog"
+          :result="result"
+          @close="dialog = false"
+        />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import ResultDialog from '@/components/ResultDialog.vue';
 import { getItem, editItem } from '@/api/items';
 
 export default {
+  components: {
+    ResultDialog,
+  },
   async mounted() {
     let { data } = await getItem(this.$route.params.id);
     this.item = data.outPutValue;
     this.content = data.outPutValue.content;
-    console.log(this.content.match(/https/gi));
   },
   data() {
     return {
       item: '',
       content: '',
+      loading: false,
+      dialog: false,
+      result: '',
     };
   },
   methods: {
     async editItem() {
-      console.log(this.item.itemid);
-      let data = await editItem(this.item.itemid, this.content);
-      console.log(data);
+      this.loading = true;
+      try {
+        let data = await editItem(this.item.itemid, this.content);
+        console.log(data);
+        this.result = data.status == 200 ? 'success' : 'fail';
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.loading = false;
+        this.dialog = true;
+      }
     },
   },
 };
