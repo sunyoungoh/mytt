04fe30736ab816aa4676107692efc7c8e1c9e991
SunYoung Oh <tjsdud7379@gmail.com>
2023-05-25 18:33:49 +0900

    Update show only digital Author

--- src/views/Mypage.vue
+++ src/views/Mypage.vue
@@ -6,48 +6,60 @@
         <h3>{{ $store.state.brandName }}</h3>
         <span class="ml-1 text-body-2">{{ $store.state.brandId }}</span>
       </div>
-      <InputLabel>
-        <template #title> 네이버 메일 등록</template>
-        <template #desc>
-          디지털 상품을 발송할 때 사용되는 이메일 주소입니다.
-        </template>
-      </InputLabel>
-      <v-form ref="form" v-model="valid" lazy-validation>
-        <div class="email-input pb-3">
-          <v-text-field
-            outlined
-            ref="naverEmail"
-            placeholder="네이버 이메일을 입력하세요"
-            :rules="emailRules"
-            v-model="naverEmail"
-            hide-details="auto"
-            class="mb-2"
-            @blur="onBlur"
-          >
-          </v-text-field>
-          <v-text-field
-            outlined
-            ref="naverPassword"
-            type="password"
-            placeholder="네이버 비밀번호를 입력하세요"
-            v-model="naverPassword"
-            hide-details="auto"
-            class="mb-2"
-            @blur="onBlur"
-          >
-          </v-text-field>
-          <v-btn
-            elevation="0"
-            color="primary"
-            class="btn-save"
-            height="56"
-            block
-            :disabled="!valid"
-            @click="saveNaverAuth"
-            >저장하기
-          </v-btn>
-        </div>
-      </v-form>
+      <div class="mail-input-wrap" v-if="$store.getters.isDigitalAuthor">
+        <InputLabel>
+          <template #title> 네이버 메일 등록</template>
+          <template #desc>
+            디지털 상품을 발송할 때 사용되는 이메일 주소입니다.
+          </template>
+        </InputLabel>
+
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <div class="email-input pb-3">
+            <v-text-field
+              outlined
+              ref="naverEmail"
+              placeholder="네이버 이메일을 입력하세요"
+              :rules="emailRules"
+              v-model="naverEmail"
+              hide-details="auto"
+              class="mb-2"
+              @blur="onBlur"
+            >
+            </v-text-field>
+            <v-text-field
+              outlined
+              ref="naverPassword"
+              type="password"
+              placeholder="네이버 비밀번호를 입력하세요"
+              v-model="naverPassword"
+              hide-details="auto"
+              class="mb-2"
+              @blur="onBlur"
+            >
+            </v-text-field>
+            <v-btn
+              elevation="0"
+              color="primary"
+              class="btn-save"
+              height="56"
+              block
+              :disabled="!valid"
+              @click="saveNaverAuth"
+              >저장하기
+            </v-btn>
+            <v-btn
+              small
+              text
+              color="red"
+              class="px-2 mt-2"
+              href="https://www.craft.do/s/O3QCKFdW2Ip10m"
+              target="_blank"
+              >메일 발송 기능 관련 안내 보러 가기 >
+            </v-btn>
+          </div>
+        </v-form>
+      </div>
       <div class="bottom-wrap">
         <v-btn
           v-if="$store.getters.isLogin"
