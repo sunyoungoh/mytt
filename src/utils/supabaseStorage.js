48249c920b43f5b3cd0b91faba391f695227cdab
sunyoungoh <tjsdud7379@gmail.com>
2023-06-10 11:41:58 +0900

    Update save supabase image file name

--- src/utils/supabaseStorage.js
+++ src/utils/supabaseStorage.js
@@ -12,7 +12,7 @@ export const createImageUrl = async file => {
     const key = uuidv4();
     const bucket = 'images'; // supabase 에 미리 만들어둔 public 버킷 이름
 
-    await supabase.storage.from(bucket).upload(key, blob, {
+    await supabase.storage.from(bucket).upload(`${key}.jpg`, blob, {
       cacheControl: '3600',
     });
 
