4f35659b74e105f05581522a48ea77455f123ade
sunyoungoh <tjsdud7379@gmail.com>
2023-06-11 14:26:22 +0900

    Fix bug get Public Url

--- src/utils/supabaseStorage.js
+++ src/utils/supabaseStorage.js
@@ -11,13 +11,14 @@ export const createImageUrl = async file => {
     }
     const key = uuidv4();
     const bucket = 'images'; // supabase 에 미리 만들어둔 public 버킷 이름
-
-    await supabase.storage.from(bucket).upload(`${key}.jpg`, blob, {
+    const fileName = `${key}.jpg`;
+    await supabase.storage.from(bucket).upload(fileName, blob, {
       cacheControl: '3600',
     });
 
-    const { publicUrl } = await supabase.storage.from(bucket).getPublicUrl(key)
-      .data;
+    const { publicUrl } = await supabase.storage
+      .from(bucket)
+      .getPublicUrl(fileName).data;
     console.log(publicUrl);
 
     return publicUrl;
